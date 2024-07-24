474717e264159ae445e05b14192e7d793c201a04
Yuruch <fuceqwertyt@gmail.com>
2024-07-24 11:06:11 +0300

    add tags page, refactor templates & add urls and views

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from todoapp.views import TaskListView
+from todoapp.views import TaskListView, TagListView
 
 urlpatterns = [
-    path("home/", TaskListView.as_view(), name="home"),
+    path("", TaskListView.as_view(), name="home"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "todo"
--- todoapp/views.py
+++ todoapp/views.py
@@ -2,10 +2,16 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import generic
 
-from todoapp.models import Task
+from todoapp.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
-    queryset = Task.objects.prefetch_related("task")
+    queryset = Task.objects.prefetch_related("tag")
     fields = "__all__"
-    template_name = "Home"
+    template_name = "todoapp/home.html"
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    fields = "__all__"
+
