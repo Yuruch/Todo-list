446fab348970b43ca77f3795f8476f87558d1842
Yuruch <fuceqwertyt@gmail.com>
2024-07-24 10:07:58 +0300

    add view for homa page

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todoapp.models import Task
+
+
+class TaskListView(generic.ListView):
+    queryset = Task.objects.prefetch_related("task")
+    fields = "__all__"
+    template_name = "Home"
